2a137f4e7ca9bcb3c708b592fb5f12caab1875bd
Rara <tiarasanir@gmail.com>
2025-05-05 15:21:35 +0700

    update

---
+++ application/modules/time_attendance/views/lembur_menu/_detail.php
@@ -0,0 +1,60 @@
+<div class="row">
+	
+	<div class="col-md-6 col-sm-12 col-xs-12">
+		<div class="row">
+			<label class="col-md-4 col-sm-4 col-xs-4 control-label no-padding-right">Date</label>
+			<div class="col-md-8 col-sm-8 col-xs-8">
+				: <span class="date"></span>
+			</div>
+		</div>
+		<div class="row">
+			<label class="col-md-4 col-sm-4 col-xs-4 control-label no-padding-right">Date Time Start</label>
+			<div class="col-md-8 col-sm-8 col-xs-8">
+				: <span class="datetime_start"></span>
+			</div>
+		</div>
+		<div class="row">
+			<label class="col-md-4 col-sm-4 col-xs-4 control-label no-padding-right">Num of Hour</label>
+			<div class="col-md-8 col-sm-8 col-xs-8">
+				: <span class="num_of_hour"></span>
+			</div>
+		</div>
+		<div class="row">
+			<label class="col-md-4 col-sm-4 col-xs-4 control-label no-padding-right">Reason</label>
+			<div class="col-md-8 col-sm-8 col-xs-8">
+				: <span class="reason"></span>
+			</div>
+		</div>
+		
+	</div>
+
+
+
+	<div class="col-md-6 col-sm-12 col-xs-12">
+		<div class="row">
+			<label class="col-md-4 col-sm-4 col-xs-4 control-label no-padding-right">Employee</label>
+			<div class="col-md-8 col-sm-8 col-xs-8">
+				: <span class="employee"></span>
+			</div>
+		</div>
+		<div class="row">
+			<label class="col-md-4 col-sm-4 col-xs-4 control-label no-padding-right">Date Time End</label>
+			<div class="col-md-8 col-sm-8 col-xs-8">
+				: <span class="datetime_end"></span>
+			</div>
+		</div>
+		<div class="row">
+			<label class="col-md-4 col-sm-4 col-xs-4 control-label no-padding-right">Amount</label>
+			<div class="col-md-8 col-sm-8 col-xs-8">
+				: <span class="amount"></span>
+			</div>
+		</div>
+		<div class="row">
+			<label class="col-md-4 col-sm-4 col-xs-4 control-label no-padding-right">Status</label>
+			<div class="col-md-8 col-sm-8 col-xs-8">
+				: <span class="status"></span>
+			</div>
+		</div>
+		
+	</div>
+</div>
---
+++ application/modules/time_attendance/views/lembur_menu/_field.php
@@ -0,0 +1,45 @@
+<div class="row">
+	
+	<div class="col-md-6 col-sm-12">
+		<div class="form-group">
+			<label class="col-md-4 control-label no-padding-right">Date</label>
+			<div class="col-md-8">
+				<?=$txtdate;?>
+			</div>
+		</div>
+		
+		<div class="form-group">
+			<label class="col-md-4 control-label no-padding-right">Date Time Start</label>
+			<div class="col-md-8">
+				<?=$txtdatetimestart;?>
+			</div>
+		</div>
+		<div class="form-group">
+			<label class="col-md-4 control-label no-padding-right">Reason</label>
+			<div class="col-md-8">
+				<?=$txtreason;?>
+			</div>
+		</div>
+		<span style="color:red" class="sisa_cuti"></span>
+		
+	</div>
+	<div class="col-md-6 col-sm-12">
+		<div class="form-group">
+			<label class="col-md-4 control-label no-padding-right">Employee</label>
+			<div class="col-md-8">
+				<?=$selemployee;?>
+			</div>
+		</div>
+		<div class="form-group">
+			<label class="col-md-4 control-label no-padding-right">Date Time End</label>
+			<div class="col-md-8">
+				<?=$txtdatetimeend;?>
+			</div>
+		</div>
+		
+	</div>
+</div>
+
+
+
+								
