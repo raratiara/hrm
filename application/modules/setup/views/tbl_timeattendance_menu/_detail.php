0c2a347994dd77e807b7aaf7cfa09c221a0385cb
Rara <tiarasanir@gmail.com>
2025-05-05 10:08:01 +0700

    update

---
+++ application/modules/setup/views/tbl_timeattendance_menu/_detail.php
@@ -0,0 +1,36 @@
+<div class="row">
+	
+	<div class="col-md-6 col-sm-12 col-xs-12">
+		<div class="row">
+			<label class="col-md-4 col-sm-4 col-xs-4 control-label no-padding-right">Name</label>
+			<div class="col-md-8 col-sm-8 col-xs-8">
+				: <span class="name"></span>
+			</div>
+		</div>
+		<div class="row">
+			<label class="col-md-4 col-sm-4 col-xs-4 control-label no-padding-right">Email</label>
+			<div class="col-md-8 col-sm-8 col-xs-8">
+				: <span class="email"></span>
+			</div>
+		</div>
+		
+	</div>
+
+
+
+	<div class="col-md-6 col-sm-12 col-xs-12">
+		<div class="row">
+			<label class="col-md-4 col-sm-4 col-xs-4 control-label no-padding-right">Username</label>
+			<div class="col-md-8 col-sm-8 col-xs-8">
+				: <span class="username"></span>
+			</div>
+		</div>
+		<div class="row">
+			<label class="col-md-4 col-sm-4 col-xs-4 control-label no-padding-right">Status</label>
+			<div class="col-md-8 col-sm-8 col-xs-8">
+				: <span class="status"></span>
+			</div>
+		</div>
+		
+	</div>
+</div>
