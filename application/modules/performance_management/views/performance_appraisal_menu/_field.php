829f310203c64e6f8275206058ed30052c4b426a
Rara <tiarasanir@gmail.com>
2025-05-06 17:13:03 +0700

    update

---
+++ application/modules/performance_management/views/performance_appraisal_menu/_field.php
@@ -0,0 +1,27 @@
+<div class="row">
+	
+	<div class="col-md-6 col-sm-12">
+		
+		<div class="form-group">
+			<label class="col-md-4 control-label no-padding-right"> Employee</label>
+			<div class="col-md-8">
+				<?=$selemployee;?>
+			</div>
+		</div>
+		
+	</div>
+	<div class="col-md-6 col-sm-12">
+		<div class="form-group">
+			<label class="col-md-4 control-label no-padding-right">Year</label>
+			<div class="col-md-8">
+				<?=$txtyear;?>
+			</div>
+		</div>
+		
+	</div>
+</div>
+
+
+
+
+								
