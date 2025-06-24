16fac01e04f8723b415b4875b852f2bc1f61e37b
Rara <tiarasanir@gmail.com>
2025-06-24 17:29:25 +0700

    update

---
+++ application/modules/hr_menu/views/mpp_menu/_detail.php
@@ -0,0 +1,39 @@
+<div class="row">
+	
+	<div class="col-md-6 col-sm-12 col-xs-12">
+		<div class="row">
+			<label class="col-md-4 col-sm-4 col-xs-4 control-label no-padding-right">Year</label>
+			<div class="col-md-8 col-sm-8 col-xs-8">
+				: <span class="year"></span>
+			</div>
+		</div>
+		<div class="row">
+			<label class="col-md-4 col-sm-4 col-xs-4 control-label no-padding-right">Section</label>
+			<div class="col-md-8 col-sm-8 col-xs-8">
+				: <span class="section"></span>
+			</div>
+		</div>
+		<div class="row">
+			<label class="col-md-4 col-sm-4 col-xs-4 control-label no-padding-right">Level</label>
+			<div class="col-md-8 col-sm-8 col-xs-8">
+				: <span class="level"></span>
+			</div>
+		</div>
+		<div class="row">
+			<label class="col-md-4 col-sm-4 col-xs-4 control-label no-padding-right">Headcount</label>
+			<div class="col-md-8 col-sm-8 col-xs-8">
+				: <span class="headcount"></span>
+			</div>
+		</div>
+		
+	</div>
+	<div class="col-md-12 col-sm-12 col-xs-12">
+		<div class="row">
+			<label class="col-md-2 col-sm-4 col-xs-4 control-label no-padding-right">Description</label>
+			<div class="col-md-10 col-sm-8 col-xs-8">
+				: <span class="notes"></span>
+			</div>
+		</div>
+		
+	</div>
+</div>
---
+++ application/modules/hr_menu/views/mpp_menu/_field.php
@@ -0,0 +1,45 @@
+<div class="row">
+	
+	<div class="col-md-12 col-sm-12">
+		<div class="form-group">
+			<label class="col-md-2 control-label no-padding-right">Year</label>
+			<div class="col-md-10">
+				<?=$txtyear;?>
+			</div>
+		</div>
+
+		<div class="form-group">
+			<label class="col-md-2 control-label no-padding-right">Section</label>
+			<div class="col-md-10">
+				<?=$selsection;?>
+			</div>
+		</div>
+		
+		<div class="form-group">
+			<label class="col-md-2 control-label no-padding-right">Level</label>
+			<div class="col-md-10">
+				<?=$seljoblevel;?>
+			</div>
+		</div>
+		<div class="form-group">
+			<label class="col-md-2 control-label no-padding-right">Headcount</label>
+			<div class="col-md-10">
+				<?=$txtheadcount;?>
+			</div>
+		</div>
+		
+	</div>
+	<div class="col-md-12 col-sm-12">
+		<div class="form-group">
+			<label class="col-md-2 control-label no-padding-right">Description </label>
+			<div class="col-md-10">
+				<?=$txtnotes;?>
+			</div>
+		</div>
+	</div>
+	
+</div>
+
+
+
+								
