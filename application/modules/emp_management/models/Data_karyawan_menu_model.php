426dcd3f0534293fe58a5ad81f22289bc615b841
Rara <tiarasanir@gmail.com>
2025-07-02 14:47:39 +0700

    update

--- application/modules/emp_management/models/Data_karyawan_menu_model.php
+++ application/modules/emp_management/models/Data_karyawan_menu_model.php
@@ -1190,7 +1190,7 @@ class Data_karyawan_menu_model extends MY_Model
 
 
 
-	public function import_data($list_data){
+	public function import_data($list_data){  echo "tes"; die();
 
 		if (isset($list_data[0][0]) && is_array($list_data[0][0])) {
 		    $list_data[0] = $list_data[0][0];
