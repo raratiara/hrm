21ea4b516b5fd8a96bee476244b94f2bbf065da9
Rara <tiarasanir@gmail.com>
2025-07-08 12:13:52 +0700

    update

---
+++ application/modules/documents/views/documents_menu/_field.php
@@ -0,0 +1,15 @@
+<div class="form-group">
+	<label class="col-md-3 control-label no-padding-right">Name <span class="required">*</span></label>
+	<div class="col-md-9">
+		<?=$txtname;?>
+	</div>
+</div>
+<div class="form-group">
+	<label class="col-md-3 control-label no-padding-right">File <span class="required">*</span></label>
+	<div class="col-md-9">
+		<?=$txtfile;?>
+		<div id="file-link"></div>
+		<input type="hidden" id="hdnfile" name="hdnfile">
+	</div>
+</div>
+									
