901665a21d8dd77619fda46cb80ff984b3f44101
Rara <tiarasanir@gmail.com>
2025-07-15 22:28:44 +0700

    update

---
+++ application/modules/hr_menu/views/officeinfo_menu/css_view.php
@@ -0,0 +1,18 @@
+<style type="text/css">
+[class*="col-"] .chosen-container {
+    width:98%!important;
+}
+[class*="col-"] .chosen-container .chosen-search input[type="text"] {
+    padding:2px 4%!important;
+    width:90%!important;
+    margin:5px 2%;
+}
+[class*="col-"] .chosen-container .chosen-drop {
+    width: 100%!important;
+}
+
+.modal-content{
+    width:1100px;
+    margin-left:-250px
+}
+</style>
