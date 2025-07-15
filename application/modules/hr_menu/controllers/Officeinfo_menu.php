0fcd2aaa5f9c1b6182ec64d29260d87a7c4982bc
Rara <tiarasanir@gmail.com>
2025-07-15 22:28:44 +0700

    update

---
+++ application/modules/hr_menu/controllers/Officeinfo_menu.php
@@ -0,0 +1,114 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Officeinfo_menu extends MY_Controller
+{
+	/* Module */
+ 	const  LABELMODULE				= "officeinfo_menu"; // identify menu
+ 	const  LABELMASTER				= "Menu Office Information";
+ 	const  LABELFOLDER				= "hr_menu"; // module folder
+ 	const  LABELPATH				= "officeinfo_menu"; // controller file (lowercase)
+ 	const  LABELNAVSEG1				= "hr_menu"; // adjusted 1st sub parent segment
+ 	const  LABELSUBPARENTSEG1		= "Master"; // 
+ 	const  LABELNAVSEG2				= ""; // adjusted 2nd sub parent segment
+ 	const  LABELSUBPARENTSEG2		= ""; // 
+	
+	/* View */
+	public $icon 					= 'fa-database';
+	public $tabel_header 			= ["ID","Type","Label 1","Label 2","Title","Description","Show Date Start","Show Date End"];
+
+	
+	/* Export */
+	public $colnames 				= ["ID","Type","Label 1","Label 2","Title","Description","Show Date Start","Show Date End"];
+	public $colfields 				= ["id","type","label1","label2","title","description","show_date_start","show_date_end"];
+
+	/* Form Field Asset */
+	public function form_field_asset()
+	{
+		$getdata = $this->db->query("select * from user where user_id = '".$_SESSION['id']."'")->result(); 
+		$karyawan_id = $getdata[0]->id_karyawan;
+		$whr='';
+		if($getdata[0]->id_groups != 1){ //bukan super user
+			$whr=' and id = "'.$karyawan_id.'" or direct_id = "'.$karyawan_id.'" ';
+		}
+
+
+
+		$field = [];
+
+		$field['seltype'] 			= $this->self_model->return_build_radio('', [['Event','Event'],['News','News']], 'info_type', '', 'inline');
+		$field['txtlabel1'] 		= $this->self_model->return_build_txt('','label1','label1');
+		$field['txttitle'] 			= $this->self_model->return_build_txt('','title','title');
+		$field['txtshowdatestart'] 	= $this->self_model->return_build_txt('','show_date_start','show_date_start');
+		$field['txtlabel2'] 		= $this->self_model->return_build_txt('','label2','label2');
+
+		$field['txtdescription'] 	= $this->self_model->return_build_txtarea('','description','description');
+		$field['txtshowdateend'] 	= $this->self_model->return_build_txt('','show_date_end','show_date_end');
+
+		
+		
+		return $field;
+	}
+
+	//========================== Considering Already Fixed =======================//
+ 	/* Construct */
+	public function __construct() {
+        parent::__construct();
+		# akses level
+		$akses = $this->self_model->user_akses($this->module_name);
+		define('_USER_ACCESS_LEVEL_VIEW',$akses["view"]);
+		define('_USER_ACCESS_LEVEL_ADD',$akses["add"]);
+		define('_USER_ACCESS_LEVEL_UPDATE',$akses["edit"]);
+		define('_USER_ACCESS_LEVEL_DELETE',$akses["del"]);
+		define('_USER_ACCESS_LEVEL_DETAIL',$akses["detail"]);
+		define('_USER_ACCESS_LEVEL_IMPORT',$akses["import"]);
+		define('_USER_ACCESS_LEVEL_EKSPORT',$akses["eksport"]);
+    }
+
+	/* Module */
+ 	public $folder_name				= self::LABELFOLDER."/".self::LABELPATH; // module path
+ 	public $module_name				= self::LABELMODULE;
+ 	public $model_name				= self::LABELPATH."_model";
+
+	/* Navigation */
+ 	public $parent_menu				= self::LABELFOLDER;
+ 	public $subparent_menu			= self::LABELNAVSEG1;
+ 	public $subparentitem_menu		= self::LABELNAVSEG2;
+ 	public $sub_menu 				= self::LABELMODULE;
+
+	/* Label */
+ 	public $label_parent_modul		= self::LABELFOLDER;
+ 	public $label_subparent_modul	= self::LABELSUBPARENTSEG1;
+ 	public $label_subparentitem_modul	= self::LABELSUBPARENTSEG2;
+ 	public $label_modul				= self::LABELMASTER;
+ 	public $label_list_data			= "Daftar Data ".self::LABELMASTER;
+ 	public $label_add_data			= "Tambah Data ".self::LABELMASTER;
+ 	public $label_update_data		= "Edit Data ".self::LABELMASTER;
+ 	public $label_sukses_disimpan 	= "Data berhasil disimpan";
+ 	public $label_gagal_disimpan 	= "Data gagal disimpan";
+ 	public $label_delete_data		= "Hapus Data ".self::LABELMASTER;
+ 	public $label_sukses_dihapus 	= "Data berhasil dihapus";
+ 	public $label_gagal_dihapus 	= "Data gagal dihapus";
+ 	public $label_detail_data		= "Datail Data ".self::LABELMASTER;
+ 	public $label_import_data		= "Import Data ".self::LABELMASTER;
+ 	public $label_sukses_diimport 	= "Data berhasil diimport";
+ 	public $label_gagal_diimport 	= "Import data di baris : ";
+ 	public $label_export_data		= "Export";
+ 	public $label_gagal_eksekusi 	= "Eksekusi gagal karena ketiadaan data";
+
+	//============================== Additional Method ==============================//
+
+
+ 	public function getDataEmp(){
+		$post = $this->input->post(null, true);
+		$empid = $post['empid'];
+
+		$rs =  $this->self_model->getDataEmployee($empid);
+		
+
+		echo json_encode($rs);
+	}
+
+
+
+}
---
+++ application/modules/hr_menu/models/Officeinfo_menu_model.php
@@ -0,0 +1,361 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Officeinfo_menu_model extends MY_Model
+{
+	/* Module */
+ 	protected $folder_name				= "hr_menu/officeinfo_menu";
+ 	protected $table_name 				= _PREFIX_TABLE."office_info";
+ 	protected $primary_key 				= "id";
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	// fix
+	public function get_list_data()
+	{
+		$aColumns = [
+			NULL,
+			NULL,
+			'dt.id',
+			'dt.label1',
+			'dt.label2',
+			'dt.title',
+			'dt.description',
+			'dt.type',
+			'dt.show_date_start',
+			'dt.show_date_end'
+		];
+		
+
+		$sIndexColumn = $this->primary_key;
+		$sTable = '(select * from office_info)dt';
+		
+
+		/* Paging */
+		$sLimit = "";
+		if(isset($_GET['iDisplayStart']) && $_GET['iDisplayLength'] != '-1'){
+			$sLimit = "LIMIT ".($_GET['iDisplayStart']).", ".
+			($_GET['iDisplayLength']);
+		}
+
+		/* Ordering */
+		$sOrder = "";
+		if(isset($_GET['iSortCol_0'])) {
+			$sOrder = "ORDER BY  ";
+			for ($i=0 ; $i<intval($_GET['iSortingCols']) ; $i++){
+				if($_GET['bSortable_'.intval($_GET['iSortCol_'.$i])] == "true"){
+					$srcCol = $aColumns[ intval($_GET['iSortCol_'.$i])];
+					$findme   = ' as ';
+					$pos = strpos($srcCol, $findme);
+					if ($pos !== false) {
+						$pieces = explode($findme, trim($srcCol));
+						$sOrder .= trim($pieces[0])."
+						".($_GET['sSortDir_'.$i]) .", ";
+					} else {
+						$sOrder .= $aColumns[ intval($_GET['iSortCol_'.$i])]."
+						".($_GET['sSortDir_'.$i]) .", ";
+					}
+				}
+			}
+
+			$sOrder = substr_replace($sOrder, "", -2);
+			if($sOrder == "ORDER BY"){
+				$sOrder = "";
+			}
+		}
+
+		/* Filtering */
+		$sWhere = " WHERE 1 = 1 ";
+		if(isset($_GET['sSearch']) && $_GET['sSearch'] != ""){
+			$sWhere .= "AND (";
+			foreach ($aColumns as $c) {
+				if($c !== NULL){
+					$srcCol = $c;
+					$findme   = ' as ';
+					$pos = strpos($srcCol, $findme);
+					if ($pos !== false) {
+						$pieces = explode($findme, trim($srcCol));
+						$sWhere .= trim($pieces[0])." LIKE '%".($_GET['sSearch'])."%' OR ";
+					} else {
+						$sWhere .= $c." LIKE '%".($_GET['sSearch'])."%' OR ";
+					}
+				}
+			}
+
+			$sWhere = substr_replace( $sWhere, "", -3);
+			$sWhere .= ')';
+		}
+
+		/* Individual column filtering */
+		for($i=0 ; $i<count($aColumns) ; $i++) {
+			if(isset($_GET['bSearchable_'.$i]) && $_GET['bSearchable_'.$i] == "true" && isset($_GET['sSearch_'.$i]) && $_GET['sSearch_'.$i] != ''){
+				if($sWhere == ""){
+					$sWhere = "WHERE ";
+				} else {
+					$sWhere .= " AND ";
+				}
+				$srcString = $_GET['sSearch_'.$i];
+				$findme   = '|';
+				$pos = strpos($srcString, $findme);
+				if ($pos !== false) {
+					$srcKey = "";
+					$pieces = explode($findme, trim($srcString));
+					foreach ($pieces as $value) {
+						if(!empty($srcKey)){
+							$srcKey .= ",";
+						}
+						$srcKey .= "'".$value."'";
+					}
+					
+					$srcCol = $aColumns[$i];
+					$findme   = ' as ';
+					$pos = strpos($srcCol, $findme);
+					if ($pos !== false) {
+						$pieces = explode($findme, trim($srcCol));
+						$sWhere .= trim($pieces[0])." IN (".$srcKey.") ";
+					} else {
+						$sWhere .= $aColumns[$i]." IN (".$srcKey.") ";
+					}
+				} else {
+					$srcCol = $aColumns[$i];
+					$findme   = ' as ';
+					$pos = strpos($srcCol, $findme);
+					if ($pos !== false) {
+						$pieces = explode($findme, trim($srcCol));
+						$sWhere .= trim($pieces[0])." LIKE '%".($srcString)."%' ";
+					} else {
+						$sWhere .= $aColumns[$i]." LIKE '%".($srcString)."%' ";
+					}
+				}
+			}
+		}
+
+		/* Get data to display */
+		$filtered_cols = array_filter($aColumns, [$this, 'is_not_null']); // Filtering NULL value
+		$sQuery = "
+		SELECT  SQL_CALC_FOUND_ROWS ".str_replace(" , ", " ", implode(", ", $filtered_cols))."
+		FROM $sTable
+		$sWhere
+		$sOrder
+		$sLimit
+		";
+		# echo $sQuery;exit;
+		$rResult = $this->db->query($sQuery)->result();
+
+		/* Data set length after filtering */
+		$sQuery = "
+			SELECT FOUND_ROWS() AS filter_total
+		";
+		$aResultFilterTotal = $this->db->query($sQuery)->row();
+		$iFilteredTotal = $aResultFilterTotal->filter_total;
+
+		/* Total data set length */
+		$sQuery = "
+			SELECT COUNT(".$sIndexColumn.") AS total
+			FROM $sTable
+		";
+		$aResultTotal = $this->db->query($sQuery)->row();
+		$iTotal = $aResultTotal->total;
+
+		$output = array(
+			"sEcho" => intval($_GET['sEcho']),
+			"iTotalRecords" => $iTotal,
+			"iTotalDisplayRecords" => $iFilteredTotal,
+			"aaData" => array()
+		);
+
+		foreach($rResult as $row)
+		{
+			$detail = "";
+			if (_USER_ACCESS_LEVEL_DETAIL == "1")  {
+				$detail = '<a class="btn btn-xs btn-success detail-btn" href="javascript:void(0);" onclick="detail('."'".$row->id."'".')" role="button"><i class="fa fa-search-plus"></i></a>';
+			}
+			$edit = "";
+			if (_USER_ACCESS_LEVEL_UPDATE == "1")  {
+				$edit = '<a class="btn btn-xs btn-primary" href="javascript:void(0);" onclick="edit('."'".$row->id."'".')" role="button"><i class="fa fa-pencil"></i></a>';
+			}
+			$delete_bulk = "";
+			$delete = "";
+			if (_USER_ACCESS_LEVEL_DELETE == "1")  {
+				$delete_bulk = '<input name="ids[]" type="checkbox" class="data-check" value="'.$row->id.'">';
+				$delete = '<a class="btn btn-xs btn-danger" href="javascript:void(0);" onclick="deleting('."'".$row->id."'".')" role="button"><i class="fa fa-trash"></i></a>';
+			}
+			
+
+			array_push($output["aaData"],array(
+				$delete_bulk,
+				'<div class="action-buttons">
+					'.$detail.'
+					'.$edit.'
+					'.$delete.'
+				</div>',
+				$row->id,
+				$row->label1,
+				$row->label2,
+				$row->title,
+				$row->description,
+				$row->type,
+				$row->show_date_start,
+				$row->show_date_end
+
+
+			));
+		}
+
+		echo json_encode($output);
+	}
+
+	// filltering null value from array
+	public function is_not_null($val){
+		return !is_null($val);
+	}		
+
+	public function delete($id= "") {
+		if (isset($id) && $id <> "") {
+			//$this->db->trans_off(); // Disable transaction
+			$this->db->trans_start(); // set "True" for query will be rolled back
+			$this->db->where([$this->primary_key => $id])->delete($this->table_name);
+			$this->db->trans_complete();
+
+			return $rs = $this->db->trans_status();
+		} else return null;
+	}  
+
+	// delete multi items action
+	public function bulk($id= "") {
+		if (is_array($id) && count($id)) {
+			$err = '';
+			foreach ($id as $pid) {
+				//$this->db->trans_off(); // Disable transaction
+				$this->db->trans_start(); // set "True" for query will be rolled back
+				$this->db->where([$this->primary_key => $pid])->delete($this->table_name);
+				$this->db->trans_complete();
+				$deleted = $this->db->trans_status();
+                if ($deleted == false) {
+					if(!empty($err)) $err .= ", ";
+                    $err .= $pid;
+                }
+			}
+			
+			$data = array();
+			if(empty($err)){
+				$data['status'] = TRUE;
+			} else {
+				$data['status'] = FALSE;
+				$data['err'] = '<br/>ID : '.$err;
+			}
+			
+			return $data;
+		} else return null;
+	}  
+
+
+	public function add_data($post) { 
+		$show_date_start 	= trim($post['show_date_start']);
+		$show_date_end 		= trim($post['show_date_end']);
+
+  		if(!empty($post['label1']) && !empty($post['label2']) && !empty($post['title'])){ 
+  			if($post['info_type'] == 'Event'){
+  				$color = 'today';
+  			}else if($post['info_type'] == 'News'){
+  				$color = 'yellow';
+  			}else{
+  				$color = 'grey'; //orange
+  			}
+
+  			$data = [
+				'label1' 			=> trim($post['label1']),
+				'label2' 			=> trim($post['label2']),
+				'color'				=> $color,
+				'title' 			=> trim($post['title']),
+				'description' 		=> trim($post['description']),
+				'type' 				=> trim($post['info_type']),
+				'show_date_start' 	=> date("Y-m-d", strtotime($show_date_start)),
+				'show_date_end' 	=> date("Y-m-d", strtotime($show_date_end)),
+				'created_at'		=> date("Y-m-d H:i:s")
+			];
+			return $rs = $this->db->insert($this->table_name, $data);
+  		}else return null;
+
+	}  
+
+	public function edit_data($post) { 
+
+		if(!empty($post['id'])){ 
+		
+			if($post['info_type'] == 'Event'){
+  				$color = 'today';
+  			}else if($post['info_type'] == 'News'){
+  				$color = 'yellow';
+  			}else{
+  				$color = 'grey'; //orange
+  			}
+
+  			$data = [
+				'label1' 			=> trim($post['label1']),
+				'label2' 			=> trim($post['label2']),
+				'color'				=> $color,
+				'title' 			=> trim($post['title']),
+				'description' 		=> trim($post['description']),
+				'type' 				=> trim($post['info_type']),
+				'show_date_start' 	=> date("Y-m-d", strtotime($show_date_start)),
+				'show_date_end' 	=> date("Y-m-d", strtotime($show_date_end)),
+				'updated_at'		=> date("Y-m-d H:i:s")
+			];
+
+			return  $rs = $this->db->update($this->table_name, $data, [$this->primary_key => trim($post['id'])]);
+		} else return null;
+	}  
+
+	public function getRowData($id) { 
+		$mTable = '(select * from office_info
+			)dt';
+
+		$rs = $this->db->where([$this->primary_key => $id])->get($mTable)->row();
+		
+		
+		
+		return $rs;
+	} 
+
+	public function import_data($list_data)
+	{
+		$i = 0;
+
+		foreach ($list_data as $k => $v) {
+			$i += 1;
+
+			$data = [
+				'employee_id' 			=> $v["B"],
+				'task' 					=> $v["C"],
+				'progress_percentage' 	=> $v["D"],
+				'parent_id' 			=> $v["E"],
+				'due_date' 				=> $v["F"],
+				'status_id' 			=> $v["G"],
+				'solve_date' 			=> $v["H"]
+			];
+
+			$rs = $this->db->insert($this->table_name, $data);
+			if (!$rs) $error .=",baris ". $v["A"];
+		}
+
+		return $error;
+	}
+
+	public function eksport_data()
+	{
+
+		$sql = "select * from office_info
+			order by id asc
+		";
+
+		$res = $this->db->query($sql);
+		$rs = $res->result_array();
+		return $rs;
+	}
+
+
+}
