b2e2337a45f74a0bc813254f180485d9ec06f297
Rara <tiarasanir@gmail.com>
2025-07-21 15:02:49 +0700

    update

--- application/modules/profile/views/profile_menu/css_view.php
+++ application/modules/profile/views/profile_menu/css_view.php
@@ -1,5 +1,5 @@
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
-
+<meta content="width=device-width, initial-scale=1" name="viewport" />
 
 <style>
   body {
