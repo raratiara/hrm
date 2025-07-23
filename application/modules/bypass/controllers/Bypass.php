a6a4823325ec47c19995f2d7dddb3792309e2cc3
Rara <tiarasanir@gmail.com>
2025-07-23 15:47:31 +0700

    update

--- application/modules/bypass/controllers/Bypass.php
+++ application/modules/bypass/controllers/Bypass.php
@@ -97,8 +97,9 @@ class Bypass extends API_Controller
 		
 
 		$message = $this->load->view(_TEMPLATE_EMAIL.$mail['template'],$data,TRUE); // load email message using view template
-		$cc = 'raratiara02@ymail.com, tiarasanir@gmail.com';
-		$bcc = 'tiarasanir@gmail.com';
+		//$cc = 'raratiara02@ymail.com, tiarasanir@gmail.com';
+		$cc='';
+		$bcc = 'raratiara02@ymail.com';
 		$this->email->from($mail['from_email'], $mail['from_name']); 
 		$this->email->to($mail['to_email'], $mail['to_name']);
 		$this->email->cc($cc);
