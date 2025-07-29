e5fab2d89a2f1eac66ace68a6895a127751d74fa
Rara <tiarasanir@gmail.com>
2025-07-29 13:57:27 +0700

    update

--- application/modules/emp_management/models/Data_karyawan_menu_model.php
+++ application/modules/emp_management/models/Data_karyawan_menu_model.php
@@ -1,6 +1,8 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+				
 class Data_karyawan_menu_model extends MY_Model
 {
 	/* Module */
