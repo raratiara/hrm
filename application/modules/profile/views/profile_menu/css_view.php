3e05450b9f5190fef66c2dd38e60480b0468facc
Rara <tiarasanir@gmail.com>
2025-08-04 08:38:38 +0700

    update

--- application/modules/profile/views/profile_menu/css_view.php
+++ application/modules/profile/views/profile_menu/css_view.php
@@ -5,13 +5,13 @@
   body {
     background-color: #f3f5fc;
     margin: 0;
-    padding: 30px;
+    /*padding: 30px;*/
 
   }
 
   .dashboard-container {
     font-family: 'Poppins', sans-serif;
-    max-width: 1000px;
+    /*max-width: 1000px;*/
     margin: auto;
     display: flex;
     flex-direction: column;
