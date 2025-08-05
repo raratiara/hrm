ba1059d7519640fa71170c4624bfbe3fb080078b
Rara <tiarasanir@gmail.com>
2025-08-05 12:09:01 +0700

    update

--- application/modules/tpl/views/main_page.php
+++ application/modules/tpl/views/main_page.php
@@ -3,6 +3,9 @@
 
 <head>
     <meta charset="utf-8" />
+    
+   
+    <link rel="icon" href="<?php echo _ASSET_LOGO_TSP; ?>" type="image/PNG" />
     <title><?php echo _TITLE; ?></title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta content="width=device-width, initial-scale=1" name="viewport" />
