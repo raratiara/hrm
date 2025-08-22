5c4d8687b9e5618a2d49d53b318e3bfdb4f26150
Rara <tiarasanir@gmail.com>
2025-08-22 12:52:25 +0700

    update

--- application/modules/time_attendance/controllers/Ijin_menu.php
+++ application/modules/time_attendance/controllers/Ijin_menu.php
@@ -38,7 +38,7 @@ class Ijin_menu extends MY_Controller
 		
 		$msemp 					= $this->db->query("select * from employees where status_id = 1 ".$whr." order by full_name asc")->result(); 
 		$field['selemployee'] 	= $this->self_model->return_build_select2me($msemp,'','','','employee','employee','','','id','full_name',' ','','','',3,'-');
-		$msleave 				= $this->db->query("select * from master_leaves")->result(); 
+		$msleave 				= $this->db->query("select * from master_leaves where name != 'Absence' ")->result(); 
 		$field['selleavetype'] 	= $this->self_model->return_build_select2me($msleave,'','','','leave_type','leave_type','','','id','name',' ','','','',3,'-');
 		$field['txtreason']		= $this->self_model->return_build_txtarea('','reason','reason');
 		$field['txtdatestart']	= $this->self_model->return_build_txt('','date_start','date_start');
