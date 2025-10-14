6e08d00837c46feac45eea94db23171e486efcbc
Rara <tiarasanir@gmail.com>
2025-10-14 17:58:10 +0700

    update

--- application/modules/compensation_benefit/views/reimbursement_menu/_field.php
+++ application/modules/compensation_benefit/views/reimbursement_menu/_field.php
@@ -26,6 +26,12 @@
 				<?=$txtnominalbilling;?>
 			</div>
 		</div>
+		<div class="form-group" id="rfuReasonEdit" style="display: none;">
+			<label class="col-md-4 control-label no-padding-right">RFU Reason </label>
+			<div class="col-md-8">
+				<span class="rfu_reason_edit"></span>
+			</div>
+		</div>
 		
 	</div>
 	<div class="col-md-6 col-sm-12">
