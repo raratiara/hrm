6af31082dc9b043043c2b7d529a8cfa7d4022c6a
Rara <tiarasanir@gmail.com>
2025-10-16 17:02:13 +0700

    update

--- application/modules/compensation_benefit/views/loan/_field.php
+++ application/modules/compensation_benefit/views/loan/_field.php
@@ -36,6 +36,7 @@
 			<label class="col-md-4 control-label no-padding-right">Cicilan per bulan </label>
 			<div class="col-md-8">
 				<?=$txt_nominal_cicilan_per_bulan;?>
+				<input type="hidden" id="nominal_cicilan_per_bulan" name="nominal_cicilan_per_bulan">
 			</div>
 		</div> 
 	</div>
@@ -66,6 +67,39 @@
 			</div>
 		</div>  
 	</div> 
+</div>
+
+
+<div class="row loan" id="listPembayaran" style="display: none;">
+    <div class="col-md-12">
+		<div class="portlet box">
+			<div class="portlet-title">
+				<div class="caption">Detail Pembayaran </div>
+				<div class="tools">
+					
+				</div>
+			</div>
+			<div class="portlet-body">
+				<div class="table-scrollable tablesaw-cont">
+				<table class="table table-striped table-bordered table-hover loan-list tablesaw tablesaw-stack" data-tablesaw-mode="stack" id="tblDetailLoan">
+					<thead>
+						<tr>
+							<th scope="col" style="width:5%; text-align: center;">Cicilan ke</th>
+							<th scope="col" style="text-align: center">Jatuh Tempo</th>
+							<th scope="col" style="text-align: center">Status</th>
+							<th scope="col" style="text-align: center">Tanggal Bayar</th>
+						</tr>
+					</thead>
+					<tbody>
+						
+					</tbody>
+					<tfoot>
+					</tfoot>
+				</table>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 
 
