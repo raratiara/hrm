c479a6d314943f80f8b3705975b4ea4f26b8029b
Rara <tiarasanir@gmail.com>
2025-10-28 18:52:02 +0700

    add meetings

---
+++ application/modules/emp_management/views/meetings_menu/_detail.php
@@ -0,0 +1,72 @@
+<div class="row">
+	
+	<div class="col-md-12 col-sm-12 col-xs-12">
+		<div class="row">
+			<label class="col-md-2 col-sm-4 col-xs-4 control-label no-padding-right">Meeting Name</label>
+			<div class="col-md-10 col-sm-8 col-xs-8">
+				: <span class="meeting_name"></span>
+			</div>
+		</div>
+	
+	</div>
+
+
+	<div class="col-md-6 col-sm-12 col-xs-12">
+		<div class="row">
+			<label class="col-md-4 col-sm-4 col-xs-4 control-label no-padding-right">Meeting Date</label>
+			<div class="col-md-8 col-sm-8 col-xs-8">
+				: <span class="meeting_date"></span>
+			</div>
+		</div>
+		<div class="row">
+			<label class="col-md-4 col-sm-4 col-xs-4 control-label no-padding-right">Start Time</label>
+			<div class="col-md-8 col-sm-8 col-xs-8">
+				: <span class="start_time" ></span>
+			</div>
+		</div>
+		<div class="row">
+			<label class="col-md-4 col-sm-4 col-xs-4 control-label no-padding-right">Meeting Room</label>
+			<div class="col-md-8 col-sm-8 col-xs-8">
+				: <span class="meeting_room" ></span>
+			</div>
+		</div>
+		<div class="row">
+			<label class="col-md-4 col-sm-4 col-xs-4 control-label no-padding-right">Participants</label>
+			<div class="col-md-8 col-sm-8 col-xs-8">
+				: <span class="participants" ></span>
+			</div>
+		</div>
+
+	</div>
+
+	<div class="col-md-6 col-sm-12 col-xs-12">
+		<div class="row">
+			<label class="col-md-4 col-sm-4 col-xs-4 control-label no-padding-right">Time</label>
+			<div class="col-md-8 col-sm-8 col-xs-8">
+				: <span class="type"></span>
+			</div>
+		</div>
+		<div class="row">
+			<label class="col-md-4 col-sm-4 col-xs-4 control-label no-padding-right">End Time</label>
+			<div class="col-md-8 col-sm-8 col-xs-8">
+				: <span class="end_time" ></span>
+			</div>
+		</div>
+		<div class="row">
+			<label class="col-md-4 col-sm-4 col-xs-4 control-label no-padding-right">Description</label>
+			<div class="col-md-8 col-sm-8 col-xs-8">
+				: <span class="description" ></span>
+			</div>
+		</div>
+		<div class="row">
+			<label class="col-md-4 col-sm-4 col-xs-4 control-label no-padding-right">Code</label>
+			<div class="col-md-8 col-sm-8 col-xs-8">
+				: <span class="code" ></span>
+			</div>
+		</div>
+
+	</div>
+
+
+
+</div>
---
+++ application/modules/emp_management/views/meetings_menu/_field.php
@@ -0,0 +1,76 @@
+<div class="row">
+	
+	<div class="col-md-12 col-sm-12">
+		<div class="form-group">
+			<label class="col-md-2 control-label no-padding-right"> Meeting Name</label>
+			<div class="col-md-10">
+				<?=$txtmeetingname;?>
+			</div>
+		</div>
+
+	</div>
+
+	<div class="col-md-6 col-sm-12">
+		<div class="form-group">
+			<label class="col-md-4 control-label no-padding-right"> Meeting Date</label>
+			<div class="col-md-8">
+				<?=$txtmeetingdate;?>
+			</div>
+		</div>
+
+		<div class="form-group" id="inpStartTime" style="display: none;">
+			<label class="col-md-4 control-label no-padding-right"> Start Time</label>
+			<div class="col-md-8">
+				<?=$txtstarttime;?>
+			</div>
+		</div>
+
+		<div class="form-group">
+			<label class="col-md-4 control-label no-padding-right"> Meeting Room</label>
+			<div class="col-md-8">
+				<?=$selmeetingroom;?>
+			</div>
+		</div>
+		<div class="form-group">
+			<label class="col-md-4 control-label no-padding-right"> Participants</label>
+			<div class="col-md-8">
+				<?=$selparticipants;?>
+			</div>
+		</div>
+	</div>
+
+	<div class="col-md-6 col-sm-12" id="inpTimes">
+		
+		<div class="form-group">
+			<label class="col-md-4 control-label no-padding-right"> Time</label>
+			<div class="col-md-8">
+				<?=$seltimes;?>
+			</div>
+		</div>
+
+		<div class="form-group" id="inpEndTime" style="display: none;">
+			<label class="col-md-4 control-label no-padding-right"> End Time</label>
+			<div class="col-md-8">
+				<?=$txtendtime;?>
+			</div>
+		</div>
+		<div class="form-group">
+			<label class="col-md-4 control-label no-padding-right"> Description</label>
+			<div class="col-md-8">
+				<?=$txtdesc;?>
+			</div>
+		</div>
+		<div class="form-group">
+			<label class="col-md-4 control-label no-padding-right"> Code</label>
+			<div class="col-md-8">
+				<?=$txtcode;?>
+			</div>
+		</div>
+	</div>
+
+
+</div>
+
+
+
+								
