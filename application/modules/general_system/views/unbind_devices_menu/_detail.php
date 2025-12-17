c98a985f3df966cad336d025084081b82bf3124d
Rara <tiarasanir@gmail.com>
2025-12-17 14:34:57 +0700

    upd

---
+++ application/modules/general_system/views/unbind_devices_menu/_detail.php
@@ -0,0 +1,30 @@
+<div class="row">
+	
+	<div class="col-md-6 col-sm-12 col-xs-12">
+		<div class="row">
+			<label class="col-md-4 col-sm-4 col-xs-4 control-label no-padding-right">Employee</label>
+			<div class="col-md-8 col-sm-8 col-xs-8">
+				: <span class="employee"></span>
+			</div>
+		</div>
+		<div class="row">
+			<label class="col-md-4 col-sm-4 col-xs-4 control-label no-padding-right">Device UUID</label>
+			<div class="col-md-8 col-sm-8 col-xs-8">
+				: <span class="device_uuid"></span>
+			</div>
+		</div>
+
+	</div>
+
+
+
+	<div class="col-md-6 col-sm-12 col-xs-12">
+		<div class="row">
+			<label class="col-md-4 col-sm-4 col-xs-4 control-label no-padding-right">Type</label>
+			<div class="col-md-8 col-sm-8 col-xs-8">
+				: <span class="type"></span>
+			</div>
+		</div>
+		
+	</div>
+</div>
