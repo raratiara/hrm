8b0b3b785b3576fd7a825855217528a1f17bf47f
Rara <tiarasanir@gmail.com>
2026-01-19 10:10:10 +0700

    upd

--- application/modules/hr_menu/views/hitung_summary_absen_os_menu/view.php
+++ application/modules/hr_menu/views/hitung_summary_absen_os_menu/view.php
@@ -9,17 +9,24 @@
 
   <style type="text/css">
   	#submitFilter{
-	  
-	    background-color: #3832d2; /*#3490dc;*/
-	  	color: white;
-	  	padding: 10px 10px;
-	  	border: none;
-	  	border-radius: 10px;
-	  	box-shadow: 0 4px 6px rgba(0, 0, 0, 0.2);
-	  	transition: all 0.2s ease-in-out;
+    background-color: #3832d2; /* #3490dc; */
+    color: white;
 
+    padding: 8px 14px;        /* kecilin tinggi & lebar */
+    font-size: 12px;          /* kecilin teks */
+    line-height: 1.2;         /* biar tinggi pas & teks center */
 
-	}
+    border: none;
+    border-radius: 6px;       /* radius ikut dikecilin */
+    box-shadow: 0 3px 4px rgba(0, 0, 0, 0.2);
+
+    transition: all 0.2s ease-in-out;
+
+    display: inline-flex;    /* center horizontal & vertical */
+    align-items: center;
+    justify-content: center;
+}
+
 
   </style>
   
@@ -27,17 +34,17 @@
 <body>
 
   	<div style="margin-top: 10px;">
-  		<div class="col-md-4 col-sm-12" style="margin-left:-30px" >
+  		<div class="col-md-6 col-sm-12" style="margin-left:-30px" >
   			<div class="form-group">
-				<label class="col-md-4 control-label no-padding-right">Employee Namee</label>
-				<div class="col-md-6">
+				<label class="col-md-4 control-label no-padding-right">Employee Name</label>
+				<div class="col-md-8">
 					<?=$selflemployee;?>
 				</div>
 			</div>
   		</div>
-  		<div class="col-md-4 col-sm-12">
+  		<div class="col-md-6 col-sm-12">
 			<div class="form-group">
-				<label class="col-md-4 control-label no-padding-right">Date Period</label>
+				<label class="col-md-3 control-label no-padding-right">Date Period</label>
 				<div class="col-md-8">
 					<input type="text" class="form-control" id="perioddate" name="perioddate">
 				</div> 
