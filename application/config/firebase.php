5af374201428738c4625952b315f97b4bf691a85
RaisulAp <raisulagung@gmail.com>
2026-01-22 15:04:34 +0700

    perbaikan

--- application/config/firebase.php
+++ application/config/firebase.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $config['firebase'] = [
-    'project_id' => 'mas-hrm-2026',
+    'project_id' => 'project-mas2026',
     'service_account' => APPPATH . 'config/firebase/service-account.json',
     'scope' => 'https://www.googleapis.com/auth/firebase.messaging',
     'token_uri' => 'https://oauth2.googleapis.com/token'
