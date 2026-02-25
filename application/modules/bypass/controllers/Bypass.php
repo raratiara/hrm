8410abad3f97ba7c9d35dae2c52db654fb9d717a
Rara <tiarasanir@gmail.com>
2026-02-25 14:28:14 +0700

    upd

--- application/modules/bypass/controllers/Bypass.php
+++ application/modules/bypass/controllers/Bypass.php
@@ -36,13 +36,6 @@ class Bypass extends API_Controller
 	}
 
 
-	public function tes(){
-
-		echo 'tesss'; die();
-
-	}
-
-
 	public function sendmail_reportabsensi()
 	{
 		
@@ -64,6 +57,39 @@ class Bypass extends API_Controller
 		$mail['attach'] = $path;
 		//$mail['key'] = $key;
 	    $output = $this->sendmail($mail);
+		// if($output){
+		// 	echo 'sukses email'; die();
+		// }else{
+		// 	echo 'gagal email'; die();
+		// 	//echo $this->email->print_debugger(['headers']); die();
+		// }	
+
+
+	}
+
+
+
+	public function tes_sendmail_reportabsensi()
+	{
+		
+		/*error_reporting(E_ALL);
+		ini_set('display_errors', 1);*/
+
+	    //$key = random_string('alnum', _ACCOUNT_KEYLENGTH);
+	    //$baseurl = 'http://localhost/_hrm';
+		$mail = array();
+		$mail['subject'] = 'Report Absensi';
+		$mail['preheader'] = '';
+		$mail['from_name'] = 'HR System';//_MAIL_SYSTEM_NAME;
+		$mail['from_email'] = 'noreply-billing@huma.net.id';//_MAIL_SYSTEM_EMAIL;
+		$mail['to_name'] = 'HR Team';
+		$mail['to_email'] = 'tiarasanir@gmail.com';
+		$mail['template'] = 'report-absensi';
+		/*$path = WRITEPATH . 'uploads/user_manual_billing.docx';*/
+		$path = _URL.'uploads/report_absensi_bulanan/export_absensi_' . date('Y-m') . '.zip'; 
+		$mail['attach'] = $path;
+		//$mail['key'] = $key;
+	    $output = $this->tes_sendmail($mail);
 		// if($output){
 		// 	echo 'sukses email'; die();
 		// }else{
@@ -117,9 +143,50 @@ class Bypass extends API_Controller
 	}
 
 
+	private function tes_sendmail($mail)
+	{
+		/*error_reporting(E_ALL);
+		ini_set('display_errors', 1);*/
+
+
+		//Load email library 
+		$this->load->library('email');
+
+		$data = array();
+		$data['bln'] = date('F'); // July
+		$data['thn'] = date('Y'); // 2025
+		$data['preheader'] = $mail['preheader'];
+		$data['corp'] = _COMPANY_NAME;
+		$data['account_title'] = _ACCOUNT_TITLE;
+		$data['link_site'] = _URL;
+		$data['link_logo'] = _URL.'public/assets/images/logo/gerbangdata.PNG';
+		//_ASSET_LOGO; //'http://localhost/_hrm/public/assets/images/logo/gerbangdata.jpg';//_ASSET_LOGO;//_ASSET_LOGO_FRONT;
+		
+
+		$message = $this->load->view(_TEMPLATE_EMAIL.$mail['template'],$data,TRUE); // load email message using view template
+		$cc = '';
+		$bcc = '';
+		$this->email->from($mail['from_email'], $mail['from_name']); 
+		$this->email->to($mail['to_email'], $mail['to_name']);
+		$this->email->cc($cc);
+		$this->email->bcc($bcc);
+		$this->email->subject($mail['subject']); 
+		$this->email->message($message); 
+		$this->email->attach($mail['attach'],'attachment'); 
+	   
+		 //Send mail 
+		 if($this->email->send()) { echo 'sukses email'; die();
+			return true; 
+		 } else { /*echo 'gagal'; die();*/
+			/*return false; */
+			show_error($this->email->print_debugger());
+		 }
+	}
+
+
 
 	/// download report absensi stiap tgl 26 jam 8 pagi
-	public function downloadAbsenceReport(){
+	public function downloadAbsenceReport(){ 
 		/*error_reporting(E_ALL);
 		ini_set('display_errors', 1);*/
 
@@ -430,6 +497,311 @@ class Bypass extends API_Controller
 	}
 
 
+	public function tes_downloadAbsenceReport(){ 
+		/*error_reporting(E_ALL);
+		ini_set('display_errors', 1);*/
+
+
+		//$dateNow = date('Y-m-d'); //'2025-07-25';
+		$dateNow = $_GET['date'];
+
+		$timestamp = strtotime($dateNow);
+		$yearPrev = date("Y", strtotime("-1 month", $timestamp));
+		$monthPrev = date("m", strtotime("-1 month", $timestamp));
+		$dateFrom = $yearPrev . '-' . $monthPrev . '-25';
+		$dateTo = date('Y-m-25', strtotime($dateNow));
+
+		//tgl 25 bln kemarin SAMPAI tgl 25 bulan ini
+		$bln = date('F'); // July
+		$thn = date('Y'); // 2025
+		$periode = $bln.' '.$thn;
+
+
+		$where_date=" and (a.date_attendance between '".$dateFrom."' and '".$dateTo."') ";
+		
+
+		$where_emp=""; 
+		
+
+
+		$groupedByDivision = [];
+		$emp_absen = $this->db->query("select distinct(a.employee_id), b.division_id from time_attendances a left join employees b on b.id = a.employee_id where b.status_id = 1 ".$where_emp.$where_date." ")->result();
+		foreach ($emp_absen as $rowemp_absen) {
+		    $groupedByDivision[$rowemp_absen->division_id][] = $rowemp_absen->employee_id;
+		}
+
+		$zip = new ZipArchive();
+		
+		$zipFilename = FCPATH . 'uploads/report_absensi_bulanan/export_absensi_' . date('Y-m') . '.zip';
+		$zip->open($zipFilename, ZipArchive::CREATE | ZipArchive::OVERWRITE);
+
+
+		foreach ($groupedByDivision as $divisionId => $employeeIds) {
+			// CLEAR/RESET DATA SEBELUM MENGISI UNTUK DIVISI BERIKUTNYA
+    		unset($valSummary, $valrows, $valfooter, $dataSheets);
+
+		    ob_start(); // mulai buffer output
+
+		   
+
+			echo '<?xml version="1.0"?>
+			<Workbook xmlns="urn:schemas-microsoft-com:office:spreadsheet"
+			 xmlns:o="urn:schemas-microsoft-com:office:office"
+			 xmlns:x="urn:schemas-microsoft-com:office:excel"
+			 xmlns:ss="urn:schemas-microsoft-com:office:spreadsheet">
+			 <Styles>
+			   <Style ss:ID="TitleStyle">
+			     <Font ss:Bold="1" ss:Size="14"/>
+			   </Style>
+			   <Style ss:ID="SubTextStyle">
+			     
+			   </Style>
+			   <Style ss:ID="HeaderStyle">
+			     <Font ss:Bold="1"/>
+			     <Interior ss:Color="#D3D3D3" ss:Pattern="Solid"/>
+			   </Style>
+			 </Styles>';
+
+
+			$dataSheets = [];
+
+			$emp_absen = $this->db->query("select distinct(a.employee_id), b.division_id, c.name as division_name, b.full_name from time_attendances a left join employees b on b.id = a.employee_id left join divisions c on c.id = b.division_id where b.status_id = 1 and b.division_id = '".$divisionId."' ".$where_emp.$where_date." order by b.full_name asc ")->result(); 
+			if(count($emp_absen) != 0){ 
+				$no=1;
+				
+				foreach($emp_absen as $rowemp_absen){
+					
+					$sql = 'select a.*, b.full_name, if(a.is_late = "Y","Late", "") as "is_late_desc", 
+								(case 
+								when a.leave_type != "" then concat("(",c.name,")") 
+								when a.is_leaving_office_early = "Y" then "Leaving Office Early"
+								else ""
+								end) as is_leaving_office_early_desc
+								, d.name as branch_name, e.full_name as direct_name
+								,(case when a.leave_absences_id is not null then "1" else "" end) as cuti 
+								,(case when a.leave_absences_id is null and a.date_attendance_in is not null then "1" else "" end) as masuk 
+								,(case when a.leave_absences_id is null and a.date_attendance_in is not null and a.work_location = "onsite" then "1" else "" end) as piket
+								,(case when a.leave_absences_id is null and a.date_attendance_in is not null and a.work_location = "wfh" then "1" else "" end) as wfh
+								, a.notes as keterangan
+								from time_attendances a left join employees b on b.id = a.employee_id
+								left join master_leaves c on c.id = a.leave_type
+								left join branches d on d.id = b.branch_id
+								left join employees e on e.id = b.direct_id
+								where a.employee_id = "'.$rowemp_absen->employee_id.'" '.$where_date.'
+				   			ORDER BY id ASC
+					';
+
+					$res = $this->db->query($sql);
+					$data = $res->result();
+
+					
+					$ttl_cuti=0; $ttl_masuk=0; $ttl_piket=0; $ttl_wfh=0;
+					$valrows=[]; $valfooter=[];
+					foreach($data as $rowdata){ 
+						
+						$valrows[] = [
+							$rowdata->date_attendance,
+							$rowdata->cuti,
+							$rowdata->masuk,
+							$rowdata->piket,
+							$rowdata->wfh,
+							$rowdata->keterangan
+						];
+
+
+						if($rowdata->cuti != ''){
+							$ttl_cuti += $rowdata->cuti;
+						}
+						if($rowdata->masuk != ''){
+							$ttl_masuk += $rowdata->masuk;
+						}
+						if($rowdata->piket != ''){
+							$ttl_piket += $rowdata->piket;
+						}
+						if($rowdata->wfh != ''){
+							$ttl_wfh += $rowdata->wfh;
+						}
+
+					}
+
+					$valSummary[] = [
+						$no,
+						$data[0]->full_name,
+						$ttl_cuti,
+						$ttl_masuk,
+						$ttl_piket,
+						$ttl_wfh
+					];
+
+					$valfooter[] = [
+						'Total',
+						$ttl_cuti,
+						$ttl_masuk,
+						$ttl_piket,
+						$ttl_wfh
+					];
+					
+
+					$dataSheets[$data[0]->full_name] = [
+				        'title' => 'DATA ABSENSI/ACTIVITY KARYAWAN',
+				        'headers' => ['Tanggal', 'Cuti', 'Masuk', 'Piket', 'WFH', 'Keterangan'],
+				        'rows' => $valrows, /*[
+				            ['2025-06-12', '1', '4', '1', '7', ''],
+				            ['2025-06-12', '3', '2', '0', '2', ''],
+				        ],*/
+				        'subtitle' => [
+				        	['Nama', $data[0]->full_name],
+				            ['Area', $data[0]->branch_name],
+				            ['Leader', $data[0]->direct_name],
+				            ['Periode', $periode],
+				        ],
+				        'footer' => $valfooter
+				    ];
+
+				    $no++;
+				}
+
+
+				if($where_emp==""){ //ada sheet summary
+					$dataSheets['Summary'] = [
+				        'title' => 'DATA ABSENSI/ACTIVITY KARYAWAN',
+				        'headers' => ['No', 'Nama', 'Cuti', 'Masuk', 'Piket', 'WFH'],
+				        'rows' => $valSummary, 
+				        'subtitle' => [
+				        	['Division', $rowemp_absen->division_name],
+				            ['Area', ''],
+				            ['Leader', ''],
+				            ['Periode', $periode],
+				        ],
+				        'footer' => []	    
+					];
+				}
+				
+				if($where_emp==""){ //ada sheet summary, tampikan di paling depan
+					// Ambil sheet terakhir
+					$lastKey = array_key_last($dataSheets);
+					$lastSheet = [$lastKey => $dataSheets[$lastKey]];
+
+					// Hapus dari array asli
+					unset($dataSheets[$lastKey]);
+
+					// Gabungkan ulang: sheet terakhir jadi pertama
+					$dataSheets = $lastSheet + $dataSheets;
+				}
+
+			}else{ //tidak ada data
+				$dataSheets['Summary'] = [
+			        'title' => 'DATA ABSENSI/ACTIVITY KARYAWAN',
+			        'headers' => ['No', 'Nama', 'Cuti', 'Masuk', 'Piket', 'WFH'],
+			        'rows' => [
+				            ['No Data', 'No Data', 'No Data', 'No Data', 'No Data', 'No Data']
+				        ], 
+			        'subtitle' => [
+			            ['Area', ''],
+			            ['Leader', ''],
+			            ['Periode', $periode],
+			        ],
+			        'footer' => []	    
+				];
+			}
+
+			
+
+			foreach ($dataSheets as $sheetName => $sheetData) {
+			    echo '<Worksheet ss:Name="' . htmlspecialchars($sheetName) . '">';
+			    echo '<Table>';
+
+			    // Tambahkan kata-kata di atas (judul)
+			    echo '<Row>';
+			    echo '<Cell ss:MergeAcross="' . (count($sheetData['headers']) - 1) . '" ss:StyleID="TitleStyle">';
+			    echo '<Data ss:Type="String">' . htmlspecialchars($sheetData['title']) . '</Data>';
+			    echo '</Cell>';
+			    echo '</Row>';
+
+			    // Kosongkan 1 baris (opsional)
+			    echo '<Row></Row><Row></Row>';
+
+
+				foreach ($sheetData['subtitle'] as $row_S) {
+					echo '<Row>';
+			        foreach ($row_S as $cell_S) {
+			            $type_S = is_numeric($cell_S) ? 'Number' : 'String';
+			            echo '<Cell ss:StyleID="SubTextStyle"><Data ss:Type="' . $type_S . '">' . htmlspecialchars($cell_S) . '</Data></Cell>';
+			        }
+			        echo '</Row>';
+				}
+
+
+				echo '<Row></Row><Row></Row>';
+
+
+			    // Header
+			    echo '<Row>';
+			    foreach ($sheetData['headers'] as $headerCell) {
+			        echo '<Cell ss:StyleID="HeaderStyle"><Data ss:Type="String">' . htmlspecialchars($headerCell) . '</Data></Cell>';
+			    }
+			    echo '</Row>';
+
+			    // Data rows
+			    foreach ($sheetData['rows'] as $row) {
+			        echo '<Row>';
+			        foreach ($row as $cell) {
+			            $type = is_numeric($cell) ? 'Number' : 'String';
+			            echo '<Cell><Data ss:Type="' . $type . '">' . htmlspecialchars($cell) . '</Data></Cell>';
+			        }
+			        echo '</Row>';
+			    }
+
+			    echo '<Row></Row>';
+			    foreach ($sheetData['footer'] as $row_F) {
+					echo '<Row>';
+			        foreach ($row_F as $cell_F) {
+			            $type_F = is_numeric($cell_F) ? 'Number' : 'String';
+			            echo '<Cell ss:StyleID="SubTextStyle"><Data ss:Type="' . $type_F . '">' . htmlspecialchars($cell_F) . '</Data></Cell>';
+			        }
+			        echo '</Row>';
+				}
+
+
+			    echo '</Table>';
+			    echo '</Worksheet>';
+			}
+
+			echo '</Workbook>';
+
+
+			$divname = strtolower(trim($rowemp_absen->division_name));
+			$words = explode(' ', $divname);
+			if (count($words) > 1) {
+				$divname = str_replace(" ","_",$divname);
+			}
+
+		    // Di akhir:
+		    $content = ob_get_clean(); // ambil isi output
+		    $filename = "absensi_division_" . $divname . ".xls";
+		    $zip->addFromString($filename, $content);
+		}
+
+		$zip->close();
+
+		if (file_exists($zipFilename)) {
+			$this->tes_sendmail_reportabsensi();
+		} else {
+		    echo "Gagal menyimpan file ZIP.";
+		}
+
+		/*header('Content-Type: application/zip');
+		header('Content-disposition: attachment; filename=' . basename($zipFilename));
+		header('Content-Length: ' . filesize($zipFilename));*/
+		//readfile($zipFilename); //hilangkan download di browser
+		//unlink($zipFilename); // hapus file zip setelah diunduh (opsional)
+		//exit;
+
+
+
+	}
+
+
 	// cron jalan setiap hari di jam 08.00 pagi
 	public function generate_jatah_cuti(){
 		//generate h+1 dr period end
@@ -486,6 +858,7 @@ class Bypass extends API_Controller
 		}
 
 	}
+
 
 
 	// cron jalan setiap hari di jam 08.00 pagi
@@ -548,6 +921,7 @@ class Bypass extends API_Controller
 
 	}
 
+
 	//gak pakai cron submit_absen_holiday lg, dijadikan satu sm cron submit_daily_absen
 	public function submit_absen_holiday_old(){ //jalanin setiap hari jam 8 pagi
 
@@ -583,7 +957,6 @@ class Bypass extends API_Controller
 
 
 	}
-
 
 
 	public function submit_daily_absen(){ // jalan setiap hari, jam 8 pagi
@@ -682,8 +1055,8 @@ class Bypass extends API_Controller
 
 
 	}
-	
-	
+
+
 	// cron jalanin manual utk update dan insert jatah cuti (case kalau cron otomatis tidak jalan)
 	public function help_update_insert_jatah_cuti(){
 		
@@ -916,6 +1289,7 @@ class Bypass extends API_Controller
 
 	}
 
+
 	//kirim setelah WAReminderAbsen, kasi jeda 2 menit. brarti jalanin di 11.02
 	public function re_checkStatusSendWA()
 	{ 
@@ -1008,8 +1382,6 @@ class Bypass extends API_Controller
 	        echo "Tidak ada data pending/failed.";
 	    }
 	}
-
-
 
 
 	public function checkStatusSendWA($token, $idMsgString){
@@ -1116,7 +1488,7 @@ class Bypass extends API_Controller
 					// Kalau mau jadi string dipisahkan koma
 					$idMsgString = implode(',', $idMsg);
 					$statusSend = $this->checkStatusSendWA($token, $idMsgString);
-/*print_r($statusSend); die();*/
+
 
 					//ubah jd array
 					$responseArr  = json_decode($statusSend, true);
@@ -1167,87 +1539,13 @@ class Bypass extends API_Controller
 
 
 
-	// For sending email
-	// public function tes_sendmail()
-	// {
-	// 	/*error_reporting(E_ALL);
-	// 	ini_set('display_errors', 1);*/
-
-	// 	$mail = array();
-	// 	$mail['subject'] = 'TES EMAIL';
-	// 	$mail['preheader'] = '';
-	// 	$mail['from_name'] = 'HRM-GDI System';//_MAIL_SYSTEM_NAME;
-	// 	$mail['from_email'] = 'noreply-billing@huma.net.id';//_MAIL_SYSTEM_EMAIL;
-	// 	$mail['to_name'] = 'Rara';
-	// 	$mail['to_email'] = 'tiarasanir@gmail.com';
-	// 	$mail['template'] = 'report-absensi';
-	// 	/*$path = WRITEPATH . 'uploads/user_manual_billing.docx';*/
-	// 	/*$path = _URL.'uploads/report_absensi_bulanan/export_absensi_' . date('Y-m') . '.zip'; 
-	// 	$mail['attach'] = $path;*/
 
 
-	// 	//Load email library 
-	// 	$this->load->library('email');
+	public function tes(){
 
-	// 	$data = array();
-	// 	$data['bln'] = date('F'); // July
-	// 	$data['thn'] = date('Y'); // 2025
-	// 	$data['preheader'] = $mail['preheader'];
-	// 	$data['corp'] = _COMPANY_NAME;
-	// 	$data['account_title'] = _ACCOUNT_TITLE;
-	// 	$data['link_site'] = _URL;
-	// 	$data['link_logo'] = _URL.'public/assets/images/logo/gerbangdata.PNG';
-	// 	//_ASSET_LOGO; //'http://localhost/_hrm/public/assets/images/logo/gerbangdata.jpg';//_ASSET_LOGO;//_ASSET_LOGO_FRONT;
-		
+		echo 'cc'; die();
 
-	// 	$message = $this->load->view(_TEMPLATE_EMAIL.$mail['template'],$data,TRUE); // load email message using view template
-	// 	$cc = '';
-	// 	$bcc = '';
-	// 	$this->email->from($mail['from_email'], $mail['from_name']); 
-	// 	$this->email->to($mail['to_email'], $mail['to_name']);
-	// 	$this->email->cc($cc);
-	// 	$this->email->bcc($bcc);
-	// 	$this->email->subject($mail['subject']); 
-	// 	$this->email->message($message); 
-	// 	//$this->email->attach($mail['attach'],'attachment'); 
-	   
-	// 	 //Send mail 
-	// 	 if($this->email->send()) { echo 'sukses email'; die();
-	// 		return true; 
-	// 	 } else { /*echo 'gagal'; die();*/
-	// 		/*return false; */
-	// 		show_error($this->email->print_debugger());
-	// 	 }
-	// }
-
-
-
-	public function tes_sendmail()
-	{
-	    $this->load->library('emailing');
-
-	    $mail = [
-	        'subject'    => 'Pending Approval - Cash Advance',
-	        'to_name'    => 'Rara',
-	        'to_email'   => 'tiarasanir@gmail.com',
-	        'template'   => 'approval'
-	    ];
-
-	    $data = [
-	        'approver_name' => 'Rara Tiara',
-	        'app' => 'Cash Advance',
-	        'ca_num' => 'fpu323123',
-	        'link' => 'http://localhost/_hrm/general_system/sadmin_tools_access_menu_role/'
-	    ];
-
-	    if ($this->emailing->send($mail, $data)) {
-	        echo 'sukses email';
-	    } else {
-	        echo 'gagal kirim email';
-	    }
 	}
-
-
 
 
 
