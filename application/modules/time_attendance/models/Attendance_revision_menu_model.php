044508b14da56f3f575ab224e5778c234f62b741
Rara <tiarasanir@gmail.com>
2026-04-27 16:36:43 +0700

    upd

--- application/modules/time_attendance/models/Attendance_revision_menu_model.php
+++ application/modules/time_attendance/models/Attendance_revision_menu_model.php
@@ -266,7 +266,7 @@ class Attendance_revision_menu_model extends MY_Model
 				$detail = '<a class="btn btn-xs btn-success detail-btn" style="background-color: #112D80; border-color: #112D80;" href="javascript:void(0);" onclick="detail('."'".$row->id."'".')" role="button"><i class="fa fa-search-plus"></i></a>';
 			}
 			$edit = "";
-			if (_USER_ACCESS_LEVEL_UPDATE == "1")  {
+			if (_USER_ACCESS_LEVEL_UPDATE == "1" && $row->status == 'Waiting Approval')  {
 				
 				$edit = '<a class="btn btn-xs btn-primary" style="background-color: #FFA500; border-color: #FFA500;" href="javascript:void(0);" onclick="edit('."'".$row->id."'".')" role="button"><i class="fa fa-pencil"></i></a>';
 			}
